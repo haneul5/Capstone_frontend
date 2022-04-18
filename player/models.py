09cb28045b93d2d21932edf5288766b11155ba67
hnOh <dhgksmf423@gmail.com>
2022-04-18 16:39:17 +0900

    author 추가

--- player/models.py
+++ player/models.py
@@ -1,8 +1,6 @@
 from django.db import models
-
-# Create your models here.
-
-from django.db import models
+from django.contrib.auth.models import User # 다대일 관계 구현
+import os
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
@@ -14,6 +12,8 @@ class Post(models.Model):
     #file_upload = models.FileField(upload_to= 'player/files/%Y/%m/%d/', blank=True)
     created_at = models.DateTimeField(auto_now_add=True) #작성시간 = 현재시간 고정
     update_at = models.DateTimeField(auto_now=True) #얘로 업데이트 시간 ㅇㅇ
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE) # 작성자가 삭제되면 해당 게시물도 다 삭제하게 함
 
     def __str__(self):
         return f'[{self.pk}]{self.title}'
