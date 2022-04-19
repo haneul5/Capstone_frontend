b3e4162b1d39621773b2fd3c66194e303fcb06c0
hnOh <dhgksmf423@gmail.com>
2022-04-19 14:27:38 +0900

    포스트 목록에 카테고리 기능 추가

--- player/views.py
+++ player/views.py
@@ -1,10 +1,17 @@
+from unicodedata import category
 from django.shortcuts import render
 from django.views.generic import ListView , DetailView
-from .models import Post
+from .models import Post, Category
 
 class PostList(ListView):
     model = Post
     ordering = '-pk' #최신 글 순서대로
+
+    def get_context_data(self, **kwargs):
+        context = super(PostList, self).get_context_data()
+        context['categories'] = Category.objects.all() #카테고리 있을 경우 카운트 같은거
+        context['no_category_post_count'] = Post.objects.filter(category=None).count() #카테고리 없는 미분류 항목
+        return context
 
 class PostDetail(DetailView):
     model = Post
