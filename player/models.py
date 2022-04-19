7b46e1bbfcc1fdd0089ca2548fde94a0413cad7d
hnOh <dhgksmf423@gmail.com>
2022-04-19 15:05:56 +0900

    카테고리 누르면 해당 게시물만 나오게

--- player/models.py
+++ player/models.py
@@ -13,6 +13,9 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return f'/player/category/{self.slug}/'
+
     class Meta:
         verbose_name_plural = 'Categories'
 
--- player/views.py
+++ player/views.py
@@ -13,5 +13,32 @@ class PostList(ListView):
         context['no_category_post_count'] = Post.objects.filter(category=None).count() #카테고리 없는 미분류 항목
         return context
 
+
 class PostDetail(DetailView):
     model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDetail, self).get_context_data()
+        context['categories'] = Category.objects.all() #카테고리 있을 경우 카운트 같은거
+        context['no_category_post_count'] = Post.objects.filter(category=None).count() #카테고리 없는 미분류 항목
+        return context
+
+def category_page(request, slug):
+        #category = Category.objects.get(slug=slug)
+        if slug == 'no_category' :
+            category = '미분류'
+            post_list = Post.objects.filter(category=None)
+        else:
+            category = Category.objects.get(slug=slug)
+            post_list = Post.objects.filter(category=category)
+
+        return render(
+            request,
+            'player/post_list.html',
+            {
+                'post_list' : post_list,
+                'categories' : Category.objects.all(),
+                'no_category_post_count' : Post.objects.filter(category=None).count(),
+                'category' : category,
+            }
+        )
